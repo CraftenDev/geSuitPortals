b72876213eec5b1fa542d207fc912b0d2d8cd70d
leMaik <an@maik.tk>
2016-06-13 00:30:30 +0200

    Update to bukkit 1.9.4.

--- src/main/java/net/cubespace/geSuitPortals/tasks/PluginMessageTask.java
+++ src/main/java/net/cubespace/geSuitPortals/tasks/PluginMessageTask.java
@@ -2,24 +2,25 @@ package net.cubespace.geSuitPortals.tasks;
 
 import net.cubespace.geSuitPortals.geSuitPortals;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Collection;
 
 public class PluginMessageTask extends BukkitRunnable {
+    private final ByteArrayOutputStream bytes;
 
-	private final ByteArrayOutputStream bytes;
+    public PluginMessageTask(ByteArrayOutputStream bytes) {
+        this.bytes = bytes;
+    }
 
-	public PluginMessageTask(ByteArrayOutputStream bytes) {
-		this.bytes = bytes;
-	}
-	
-	public void run() {
-			Bukkit.getOnlinePlayers()[0].sendPluginMessage(
-					geSuitPortals.INSTANCE,
-					"geSuitPortals",
-					bytes.toByteArray());
-	
-	}
-
+    public void run() {
+        Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+        if (onlinePlayers.isEmpty()) {
+            geSuitPortals.INSTANCE.getLogger().info("Tried to send a pluginMessage with an empty server. Cancelling.");
+        } else {
+            onlinePlayers.iterator().next().sendPluginMessage(geSuitPortals.INSTANCE, "geSuitBans", bytes.toByteArray());
+        }
+    }
 }
